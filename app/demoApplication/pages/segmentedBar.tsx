0042881a6f206345bcddb47fadecbb44b0d381b2
MMB2 <git@mmb2.click>
2018-01-10 06:10:25 +0100

    :recycle: add todo

--- app/demoApplication/pages/segmentedBar.tsx
+++ app/demoApplication/pages/segmentedBar.tsx
@@ -34,6 +34,7 @@ class PageSegmentedBar extends Component {
           </SegmentedBar>
           <Label text={`Selected Segment is ${this.state.value}`} />
           <Label text={`If selected segment > 2 contents of SegmentBar will change dynamically`} />
+          <Label text={`TODO: on dynamic change correctly set selectedIndex highlight`} />
           <Button text="Back" onTap={goBack} />
         </StackLayout>
       </Page>
